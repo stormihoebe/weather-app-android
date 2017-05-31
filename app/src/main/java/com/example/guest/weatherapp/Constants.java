3a1383390bdc8dca99bd073d9d1733f33f549373
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-31 11:04:20 -0700

    add open weather credientials

---
+++ app/src/main/java/com/example/guest/weatherapp/Constants.java
@@ -0,0 +1,9 @@
+package com.example.guest.weatherapp;
+
+/**
+ * Created by Guest on 5/31/17.
+ */
+
+public class Constants {
+    public static final String OPEN_WEATHER_KEY = BuildConfig.OPEN_WEATHER_KEY;
+}
